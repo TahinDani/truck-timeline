5e6092225de2064648528a2501a2d6f77c26b61b
tahin.daniel@gmail.com <tahin.daniel@gmail.com>
2021-01-22 17:44:48 +0100

    Filtering trucks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { Chart } from "react-google-charts"
+import { useEffect, useState } from 'react'
+import { Chart } from 'react-google-charts'
 import trucksData from './trucktimeline.json';
 import './App.css'
 
@@ -12,38 +12,51 @@ const timelineColumns = [
 
 function App() {
   const [orders, setOrders] = useState([])
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
     const timelineData = []
     trucksData.trucks.forEach((truck) => {
-      truck.assignedOrderId.forEach((id) => {
-        timelineData.push([
-          truck.name,
-          id,
-          new Date(trucksData.orders.find((order) => order.id === id).from),
-          new Date(trucksData.orders.find((order) => order.id === id).to)
-        ])
-      })
+      if (truck.name.toLowerCase().includes(filter.toLowerCase())) {
+        truck.assignedOrderId.forEach((id) => {
+          timelineData.push([
+            truck.name,
+            id,
+            new Date(trucksData.orders.find((order) => order.id === id).from),
+            new Date(trucksData.orders.find((order) => order.id === id).to)
+          ])
+        })
+      }
     })
     setOrders(timelineData)
-  }, [])
+  }, [filter])
+
+  const onFilterChange = (e) => {
+    setFilter(e.target.value)
+  }
 
   return (
     <div className="App">
-      Hello
-      <Chart
-        width={'100%'}
-        height={'100vh'}
-        chartType="Timeline"
-        loader={<div>Loading Chart</div>}
-        data={[timelineColumns, ...orders]}
-        options={{
-          timeline: {
-            colorByRowLabel: true,
-          },
-        }}
-        rootProps={{ 'data-testid': '7' }}
-      />
+      <div className="truck-filter">
+        Search truck: <input type="text" onChange={(e) => onFilterChange(e)} />
+      </div>
+      {orders.length > 0
+        ? <Chart
+            width={'100%'}
+            height={'100vh'}
+            chartType="Timeline"
+            loader={<div>Loading Chart</div>}
+            data={[timelineColumns, ...orders]}
+            options={{
+              timeline: {
+                colorByRowLabel: true,
+              },
+            }}
+            rootProps={{ 'data-testid': '7' }}
+          />
+        : <div className="no-result">There aren't any results with this filter</div>
+      }
+      
     </div>
   )
 }
