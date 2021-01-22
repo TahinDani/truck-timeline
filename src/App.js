6d2ee7fac3c2e1b9b0a716e3a2306ea1a1bc7994
tahin.daniel@gmail.com <tahin.daniel@gmail.com>
2021-01-22 15:47:43 +0100

    Display orders in the timeline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import { Chart } from "react-google-charts"
+import trucksData from './trucktimeline.json';
+import './App.css'
+
+const timelineColumns = [
+  { type: 'string', id: 'Truck' },
+  { type: 'string', id: 'Order ID' },
+  { type: 'date', id: 'Start' },
+  { type: 'date', id: 'End' },
+]
 
 function App() {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    const timelineData = []
+    trucksData.trucks.forEach((truck) => {
+      truck.assignedOrderId.forEach((id) => {
+        timelineData.push([
+          truck.name,
+          id,
+          new Date(trucksData.orders.find((order) => order.id === id).from),
+          new Date(trucksData.orders.find((order) => order.id === id).to)
+        ])
+      })
+    })
+    setOrders(timelineData)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      <Chart
+        width={'100%'}
+        height={'100vh'}
+        chartType="Timeline"
+        loader={<div>Loading Chart</div>}
+        data={[timelineColumns, ...orders]}
+        options={{
+          timeline: {
+            colorByRowLabel: true,
+          },
+        }}
+        rootProps={{ 'data-testid': '7' }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
